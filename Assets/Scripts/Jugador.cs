b37e6392a573e98efafb50fe7bcf60aaeff6a735
Oscar <oscarde_diego@ceroca.cat>
2024-05-10 20:20:04 +0200

    Jugador mig fet

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    private float _vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vel = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
